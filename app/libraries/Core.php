395ae103d7fa154df0a11908b1e4cd8c1f898384
Alex <arlix.olsen@gmail.com>
2021-06-29 07:30:24 +0100

    added Pages.php controller, added controller logic to default to pages, added logic to check for other controllers if passed as an argument

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -11,10 +11,24 @@
 		protected $params = [];
 
 		public function __construct(){
-			$this->getUrl();
+			$url = $this->getUrl();
+
+			// check if the first item in the array is a controller that exists
+			if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+				$this->currentController = ucwords($url[0]);
+				unset($url[0]);
+			}
+			// require and instantiate the controller
+			require_once '../app/controllers/' . $this->currentController . '.php';
+			$this->currentController = new $this->currentController;
 		}
 
 		public function getUrl(){
-			echo $_GET['url'];
+			if(isset($_GET['url'])){
+				$url = rtrim($_GET['url'], '/');
+				$url = filter_var($url, FILTER_SANITIZE_URL);
+				$url = explode('/', $url);
+				return $url;
+			}
 		}
 	}
