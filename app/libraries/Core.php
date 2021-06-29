a59748780f420828a2e2ee3647fbe03a6a77d7b5
Alex <arlix.olsen@gmail.com>
2021-06-29 07:16:27 +0100

    fixed htaccess, added core class, added init of core from index.php

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -6,5 +6,15 @@
 	 */
 
 	class Core {
-		
+		protected $currentController = 'Pages';
+		protected $currentMethod = 'index';
+		protected $params = [];
+
+		public function __construct(){
+			$this->getUrl();
+		}
+
+		public function getUrl(){
+			echo $_GET['url'];
+		}
 	}
