7d13beb8103fd29f44584a3bf811f324d10c204b
Alex <arlix.olsen@gmail.com>
2021-07-12 09:43:14 +0100

    added autoload for libraries

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+    // app root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // url root
+    define('URLROOT', 'http://localhost/vuln_mvc');
+
+    // site name
+    define('SITENAME', 'VulnMVC');
